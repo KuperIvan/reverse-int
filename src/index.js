bd6d530ad20f6ca85cf2d7609c599a74d263800d
Ivan Kuper <wano9715@gmail.com>
2021-03-14 17:22:17 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  let res = n.toString().split("");
+  let newString;
+
+  !res.includes("-")
+    ? (newString = res.reverse().join(""))
+    : (newString = res.slice(1).reverse().join(""));
+  return newString;
+};
